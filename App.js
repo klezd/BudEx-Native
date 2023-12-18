99e75ba5aac4323c70516bc5710d303d476ca314
siennad <siennad97@gmail.com>
2023-12-18 12:48:00 +0200

    Add exchange currency

--- App.js
+++ App.js
@@ -1,20 +1,126 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect, useCallback } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  // StyleSheet,
+} from "react-native";
+import ModalDropdown from "react-native-modal-dropdown";
+import { styles } from "./Styles";
 
-export default function App() {
+const App = () => {
+  const [amount, setAmount] = useState("");
+  const [fromCurrency, setFromCurrency] = useState("USD");
+  const [toCurrency, setToCurrency] = useState("EUR");
+  const [currencies, setCurrencies] = useState([]);
+  const [convertedAmount, setConvertedAmount] = useState(null);
+
+  const convertCurrency = useCallback(() => {
+    if (!amount) {
+      setConvertedAmount(null);
+      return;
+    }
+    fetch(`
+https://api.exchangerate-api.com/v4/latest/${fromCurrency}
+		`)
+      .then((response) => response.json())
+      .then((data) => {
+        const exchangeRates = data.rates;
+        const conversionRate = exchangeRates[toCurrency];
+
+        if (conversionRate) {
+          const result = parseFloat(amount) * conversionRate;
+          setConvertedAmount(result.toFixed(2));
+        } else {
+          setConvertedAmount("Invalid Currency ");
+        }
+      })
+      .catch((error) => {
+        console.error("Error converting currency: ", error);
+      });
+  }, [amount, fromCurrency, toCurrency]);
+
+  // to fetch currencies list
+  useEffect(() => {
+    fetch(`
+https://api.exchangerate-api.com/v4/latest/USD
+	`)
+      .then((response) => response.json())
+      .then((data) => {
+        const currencyList = Object.keys(data.rates);
+        setCurrencies(currencyList);
+      })
+      .catch((error) => {
+        console.error(" Error fetching currency data: ", error);
+      });
+  }, []);
+
+  useEffect(() => {
+    convertCurrency();
+  }, [convertCurrency]);
+
+  const swapCurrencies = () => {
+    // Swap the selected currencies
+    const to = toCurrency;
+    const from = fromCurrency;
+    setFromCurrency(to);
+    setToCurrency(from);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.subcontainer}>
+        <Text style={styles.header}>Currency Converter</Text>
+        <Text style={styles.label}>Amount:</Text>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            value={amount}
+            onChangeText={(text) => setAmount(text)}
+            keyboardType="numeric"
+            placeholder="Enter amount"
+            placeholderTextColor="#999"
+          />
+        </View>
+        <Text style={styles.label}>From Currency:</Text>
+        <View style={styles.inputContainer}>
+          <ModalDropdown
+            style={styles.dropdown}
+            options={currencies}
+            defaultValue={fromCurrency}
+            onSelect={(index, value) => setFromCurrency(value)}
+          />
+        </View>
+        <TouchableOpacity style={styles.swapButton} onPress={swapCurrencies}>
+          <Text style={styles.swapButtonText}>&#8646;</Text>
+        </TouchableOpacity>
+        <Text style={styles.label}>To Currency:</Text>
+        <View style={styles.inputContainer}>
+          <ModalDropdown
+            style={styles.dropdown}
+            options={currencies}
+            defaultValue={toCurrency}
+            onSelect={(index, value) => setToCurrency(value)}
+          />
+        </View>
+
+        <TouchableOpacity
+          style={styles.convertButton}
+          onPress={convertCurrency}
+        >
+          <Text style={styles.convertButtonText}>Convert</Text>
+        </TouchableOpacity>
+
+        {convertedAmount !== null && (
+          <Text style={styles.result}>
+            {amount} {fromCurrency} is
+            {convertedAmount} {toCurrency}
+          </Text>
+        )}
+      </View>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
